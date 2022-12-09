f7a2ccc551ac06f561e1f16b418622bb50f476bf
mr-kelma <mr.kelma@gmail.com>
2022-12-09 13:58:18 +0400

    Setting up a project and adding a zoom gesture

---
+++ Pinch/App/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Valery Keplin on 9.12.22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
