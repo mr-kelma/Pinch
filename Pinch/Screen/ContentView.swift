99962a4471ccf16356a8328b6c54e2708dd0f9ca
mr-kelma <mr.kelma@gmail.com>
2022-12-09 16:17:41 +0400

    Add new gestures for zoom and move image

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -12,14 +12,24 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
     
     // MARK: - FUNCTION
-
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
     // MARK: - CONTENT
     
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
+                
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -28,19 +38,32 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
-                // MARK: 1. TAP GESTURE
+                // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.linear(duration: 0.5)) {
+                                imageOffset = value.translation
+                            }
+                        }
+                        .onEnded { _ in
+                            if imageScale <= 1 {
+                               resetImageState()
+                            }
+                        }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -49,8 +72,16 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+            InfoPanelView(scale: imageScale, offset: imageOffset)
+                .padding(.horizontal)
+                .padding(.top, 30)
+            , alignment: .top
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
+
     }
 }
 
